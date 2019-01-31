4b98d43688996cb11ac611bb1fc28c4cca5a8afb
Samantha Wong <smiwong920@gmail.com>
2019-01-31 21:09:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(seconds)
+  seconds = 10 
+    while seconds > 0 
+    seconds -= 1 
+      puts "#{seconds} SECOND(S)!"
+    end
+  puts "HAPPY NEW YEAR!"
 end
